86c80ca9bc3efef0d63d78ec52b748ccc837a349
Sebastian <sepphod@gmail.com>
2023-05-07 15:05:38 +0000

    teleop added

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/workspace/udacity_mapmyworld/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/workspace/udacity_mapmyworld/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/udacity_mapmyworld/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/workspace/udacity_mapmyworld/catkin_ws/src:$ROS_PACKAGE_PATH"
